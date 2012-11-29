170c6a356b5bce6fe9f522e5ecaabc44a39e575a
Jakob Gruber <jakob.gruber@gmail.com>
2012-11-29 18:04:30 +0100

    Add a simple test suite

--- 1-prefix_sums/recursive.c
+++ 1-prefix_sums/recursive.c
@@ -1,22 +1,7 @@
+#include <check.h>
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * Application options.
- */
-struct opts {
-    int dummy;
-};
-
-/**
- * Parses command line arguments and constructs an options structure
- * accordingly.
- * Returns 0 on success, < 0 on error.
- */
-static int parse_args(int argc, char **argv, struct opts *opts);
-
-static void print_array(int *x, size_t n);
 
 /**
  * The compile-time type of the array elements. Must have an associative +
@@ -24,50 +9,14 @@ static void print_array(int *x, size_t n);
  */
 #define TYPE int
 
-/**
- * Performs an in-place prefix sums calculation on the area located at ptr with
- * len elements of TYPE.
- * Returns 0 on success, < 0 on error.
- */
-static int prefix_sums(TYPE *ptr, size_t len);
-
-int main(int argc, char **argv) {
-    struct opts opts;
-    if (parse_args(argc, argv, &opts) != 0) {
-        return EXIT_FAILURE;
-    }
-
-    printf("omp_get_max_threads() == %d\n", omp_get_max_threads());
-
-    int nrs[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    size_t len = sizeof(nrs) / sizeof(nrs[0]);
-    
-    if (prefix_sums(nrs, len) != 0) {
-        return EXIT_FAILURE;
-    }
-
-    print_array(nrs, len);
-
-    return EXIT_SUCCESS;
-}
-
-static int parse_args(int argc, char **argv, struct opts *opts) {
-    return 0;
-}
-
-static void print_array(int *x, size_t n) {
-    for (size_t i = 0; i < n; i++) {
-        printf("%02d ", x[i]);
-    }
-    printf("\n");
-}
-
 inline static int is_odd(int n) {
     return n & 0x01;
 }
 
 /**
- * The recursive prefix sums algorithm.
+ * Performs an in-place recursive prefix sums calculation on the area located
+ * at ptr with len elements of TYPE.
+ * Returns 0 on success, < 0 on error.
  */
 int prefix_sums(TYPE *x, size_t n) {
 
@@ -119,4 +68,141 @@ int prefix_sums(TYPE *x, size_t n) {
     free(y);
 
     return 0;
+}
+
+static TYPE *random_array(size_t n, int seed) {
+    TYPE *y = malloc(sizeof(TYPE) * n);
+    if (y == NULL) {
+        return y;
+    }
+
+    srand(seed);
+    for (size_t i = 0; i < n; i++) {
+        y[i] = (TYPE)rand();
+    }
+
+    return y;
+}
+
+static TYPE *prefix_sums_ref(const TYPE *x, size_t n) {
+    TYPE *y = malloc(sizeof(TYPE) * n);
+    if (y == NULL) {
+        return y;
+    }
+
+    TYPE sum = 0;
+    for (size_t i = 0; i < n; i++) {
+        sum += x[i];
+        y[i] = sum;
+    }
+
+    return y;
+}
+
+START_TEST(test_recursive_odd) {
+    TYPE nrs[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+    size_t len = sizeof(nrs) / sizeof(nrs[0]);
+
+    TYPE *ref = prefix_sums_ref(nrs, len);
+    prefix_sums(nrs, len);
+
+    fail_unless(memcmp(nrs, ref, len) == 0,
+            "Result not equal to reference implementation");
+
+    free(ref);
+}
+END_TEST
+
+START_TEST(test_recursive_even) {
+    TYPE nrs[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    size_t len = sizeof(nrs) / sizeof(nrs[0]);
+
+    TYPE *ref = prefix_sums_ref(nrs, len);
+    prefix_sums(nrs, len);
+
+    fail_unless(memcmp(nrs, ref, len) == 0,
+            "Result not equal to reference implementation");
+
+    free(ref);
+}
+END_TEST
+
+START_TEST(test_recursive_single) {
+    TYPE nrs[] = { 0 };
+    size_t len = sizeof(nrs) / sizeof(nrs[0]);
+
+    TYPE *ref = prefix_sums_ref(nrs, len);
+    prefix_sums(nrs, len);
+
+    fail_unless(memcmp(nrs, ref, len) == 0,
+            "Result not equal to reference implementation");
+
+    free(ref);
+}
+END_TEST
+
+START_TEST(test_recursive_1024) {
+    const size_t len = 1024;
+
+    TYPE *nrs = random_array(len, len);
+    TYPE *ref = prefix_sums_ref(nrs, len);
+
+    prefix_sums(nrs, len);
+
+    fail_unless(memcmp(nrs, ref, len) == 0,
+            "Result not equal to reference implementation");
+
+    free(nrs);
+    free(ref);
+}
+END_TEST
+
+START_TEST(test_recursive_16384) {
+    const size_t len = 16384;
+
+    TYPE *nrs = random_array(len, len);
+    TYPE *ref = prefix_sums_ref(nrs, len);
+
+    prefix_sums(nrs, len);
+
+    fail_unless(memcmp(nrs, ref, len) == 0,
+            "Result not equal to reference implementation");
+
+    free(nrs);
+    free(ref);
+}
+END_TEST
+
+static Suite *recursive_suite(void) {
+    Suite *s = suite_create("recursive");
+
+    TCase *tc_core = tcase_create("core");
+
+    tcase_add_test(tc_core, test_recursive_odd);
+    tcase_add_test(tc_core, test_recursive_even);
+    tcase_add_test(tc_core, test_recursive_single);
+    tcase_add_test(tc_core, test_recursive_1024);
+    tcase_add_test(tc_core, test_recursive_16384);
+
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+int main(int argc, char **argv) {
+    struct opts opts;
+    if (parse_args(argc, argv, &opts) != 0) {
+        return EXIT_FAILURE;
+    }
+
+    printf("omp_get_max_threads() == %d\n", omp_get_max_threads());
+
+    int number_failed;
+    Suite *s = recursive_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
