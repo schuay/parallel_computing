d78d208f93ecf4549fe702e0f0ed7e7b2b4f15a9
Jakob Gruber <jakob.gruber@gmail.com>
2012-12-30 19:10:47 +0100

    Implement binary search

--- 1-merge/merge.c
+++ 1-merge/merge.c
@@ -4,8 +4,20 @@
 
 #include "merge.h"
 
+#define DEBUG(...) fprintf(stderr, __VA_ARGS__)
+
+/**
+ * This function is run on a single thread and handles merging a subsequence.
+ * a points to the subsequence of length n. c points to the result array.
+ */
 static void merge_part(const TYPE *a, int n, const TYPE *b, int m, TYPE *c,
         int iteration, int partitions, perf_t *perf);
+
+/**
+ * Searches the haystack of size n for needle and returns
+ * the closest index it can find.
+ */
+static int binary_search(TYPE needle, const TYPE *haystack, int n);
 
 TYPE *merge(const TYPE *a, int n, const TYPE *b, int m, perf_t *perf)
 {
@@ -30,10 +42,6 @@ TYPE *merge(const TYPE *a, int n, const TYPE *b, int m, perf_t *perf)
     return c;
 }
 
-/**
- * This function is run on a single thread and handles merging a subsequence.
- * a points to the subsequence of length n. c points to the result array.
- */
 static void merge_part(const TYPE *a, int n, const TYPE *b, int m, TYPE *c,
         int iteration, int partitions, perf_t *perf)
 {
@@ -45,5 +53,27 @@ static void merge_part(const TYPE *a, int n, const TYPE *b, int m, TYPE *c,
     const int next_start = (iteration + 1) * size;
     const int length = (iteration == partitions - 1) ? n - start : next_start - start;
 
-    printf("iteration %d says hello. from %d, length %d\n", iteration, start, length);
+    DEBUG("iteration %d says hello. from %d, length %d, starts with %d\n",
+            iteration, start, length, a[start]);
+
+    int b_start = binary_search(a[start], b, m);
+
+    DEBUG("b partition from %d\n", b_start);
+}
+
+static int binary_search(TYPE needle, const TYPE *haystack, int n)
+{
+    int low = 0;
+    int high = n;
+
+    while (low < high) {
+        int mid = (low + high) / 2;
+        if (needle < haystack[mid]) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return low;
 }
