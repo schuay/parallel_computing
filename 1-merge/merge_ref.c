3dad695f511c270536c7484d085ae15060548519
c553db14 <c553db14@example.com>
2013-01-02 03:32:26 +0100

    Mark perf parameter in ref merge unused
    This prevents a compiler warning

--- 1-merge/merge_ref.c
+++ 1-merge/merge_ref.c
@@ -2,7 +2,7 @@
 
 const char *algorithm_name = "sequential reference";
 
-TYPE *merge(const TYPE *a, int n, const TYPE *b, int m, perf_t *perf)
+TYPE *merge(const TYPE *a, int n, const TYPE *b, int m, __attribute__((unused)) perf_t *perf)
 {
     return merge_ref(a, n, b, m);
 }
