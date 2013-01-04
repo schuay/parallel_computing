5934e6ceddbadd76c5bf7f391d5be1caab8b41f0
c553db14 <c553db14@example.com>
2013-01-04 23:37:25 +0100

    Make thread choosing work

--- 2-merge/merge.c
+++ 2-merge/merge.c
@@ -34,7 +34,7 @@ TYPE *merge_ref(const TYPE *a, int n, const TYPE *b, int m)
 TYPE *merge(const TYPE *a, int n, const TYPE *b, int m, perf_t *perf, const char *nproc) {
     TYPE *ret;
     const char *argv[] = {algorithm_name, "--nproc", nproc, 0};
-    int argc = 1;
+    int argc = 3;
 
     CilkContext *context = Cilk_init(&argc, (char **) argv);
     ret = EXPORT(merge_impl) (context, a, n, b, m, perf);
