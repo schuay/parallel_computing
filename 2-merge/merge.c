e7b7e75b40a925696a7d16c65c6398a5b0f1956d
c553db14 <c553db14@example.com>
2013-01-04 04:41:31 +0100

    Add the task parallel recursive merge

---
+++ 2-merge/merge.c
@@ -0,0 +1,44 @@
+#include "merge.h"
+
+#include <cilk.h>
+
+extern TYPE* EXPORT(merge_impl) (CilkContext *context, const TYPE *a, int n,
+        const TYPE *b, int m, perf_t *perf);
+
+void merge_seq(const TYPE *a, int n, const TYPE *b, int m, TYPE *c)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < n && j < m) {
+        c[k++] = ((a[i] < b[j]) ? a[i++] : b[j++]);
+    }
+
+    while (i < n) c[k++] = a[i++];
+    while (j < m) c[k++] = b[j++];
+}
+
+TYPE *merge_ref(const TYPE *a, int n, const TYPE *b, int m)
+{
+    TYPE *c = calloc(n + m, sizeof(TYPE));
+    if (c == NULL) {
+        return NULL;
+    }
+
+    merge_seq(a, n, b, m, c);
+
+    return c;
+}
+
+TYPE *merge(const TYPE *a, int n, const TYPE *b, int m, perf_t *perf, const char *nproc) {
+    TYPE *ret;
+    const char *argv[] = {algorithm_name, "--nproc", nproc, 0};
+    int argc = 1;
+
+    CilkContext *context = Cilk_init(&argc, (char **) argv);
+    ret = EXPORT(merge_impl) (context, a, n, b, m, perf);
+    Cilk_terminate(context);
+
+    return ret;
+}
---
+++ 2-merge/merge.h
@@ -0,0 +1,28 @@
+#include "perf.h"
+
+/**
+ * The datatype of array elements.
+ */
+#define TYPE int
+
+extern const char *algorithm_name;
+
+/**
+ * a and b are sorted arrays of lengths n and m.
+ * No element occurs twice within a and b.
+ *
+ * These two arrays are merged together. A reference to
+ * the resulting array of length n + m is returned and must
+ * be freed by the caller.
+ */
+TYPE *merge(const TYPE *a, int n, const TYPE *b, int m, perf_t *perf, const char *nproc);
+
+/**
+ * Performs the actual sequential merge within the given memory areas.
+ */
+void merge_seq(const TYPE *a, int n, const TYPE *b, int m, TYPE *c);
+
+/**
+ * The sequential reference implementation.
+ */
+TYPE *merge_ref(const TYPE *a, int n, const TYPE *b, int m);
