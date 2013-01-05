09583d476dcea60cb73eef89fc8043610f519031
Jakob Gruber <jakob.gruber@gmail.com>
2013-01-05 15:33:43 +0100

    Test concept

---
+++ 3-stencil/test.c
@@ -0,0 +1,4 @@
+/*
+ * Concept: Precompute iterations of some suitable size with our reference implementation.
+ * Then, compare these against the parallel implementation with various matrix partitions.
+ */
