82d04b8738bbaf058196b703cc02b632ba4b5810
Jakob Gruber <jakob.gruber@gmail.com>
2013-01-07 16:33:01 +0100

    Additional rough stencil function structure

--- 3-stencil/stencil.c
+++ 3-stencil/stencil.c
@@ -7,6 +7,8 @@
 
 const char *algorithm_name = "stencil";
 
+static void stencil_iteration(void) { }
+
 void stencil(matrix_t *matrix, int iters, int r, int c, perf_t *perf)
 {
     int processes, rank;
@@ -17,6 +19,16 @@ void stencil(matrix_t *matrix, int iters, int r, int c, perf_t *perf)
         fprintf(stderr, "r * c != processes, aborting\n");
         return;
     }
+
+    int m, n;
+    matrix_dims(matrix, &m, &n);
+
+    if (m % r  != 0 || n % c != 0) {
+        fprintf(stderr, "m / r, n / c must be evenly divisible, aborting\n");
+        return;
+    }
+
+    /* Construct our MPI cartography. */
 
     MPI_Comm comm;
 
@@ -33,6 +45,14 @@ void stencil(matrix_t *matrix, int iters, int r, int c, perf_t *perf)
     MPI_Cart_coords(comm, rank, NDIMS, coords);
 
     DEBUG("Rank %d has coords (%d, %d)\n", rank, coords[0], coords[1]);
+
+    /* Extract submatrices. */
+
+    /* Perform stencil iterations. */
+
+    for (int i = 0; i < iters; i++) {
+        stencil_iteration();
+    }
 
     MPI_Comm_free(&comm);
 }
