1d1f7b8f8845c5a0bef30e75236ae2f85678a5b0
Jakob Gruber <jakob.gruber@gmail.com>
2013-01-18 12:48:55 +0100

    Change all omp schedules to 'guided'
    Runtime depends on environment variables; guided seems to be the best
    overall choice.

--- 1-prefix_sums/reduction.c
+++ 1-prefix_sums/reduction.c
@@ -8,7 +8,7 @@ const char *algorithm_name = "reduction";
 
 int prefix_sums(TYPE *x, size_t n, perf_t *perf) {
     int sum = 0;
-#pragma omp parallel for schedule(runtime) reduction(+:sum)
+#pragma omp parallel for schedule(guided) reduction(+:sum)
     for (int i = 0; i < n; i++) {
         sum += x[i];
     }
